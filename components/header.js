f0b72f11a71ad322d79375df26ca95ff9f42ee7e
Long <long.la@clearlane.com>
2019-07-14 18:45:55 -0700

    init nextjs and pages

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+const Header = ()=>(
+    <nav>
+        <Link href="#thong-diep">Thong Diep</Link>
+        <Link href="#chia-se">Chia Se</Link>
+    </nav>
+);
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+import ThongDiep from "./thong-diep";
+import ChiaSe from "./chia-se";
+
+const Index = ()=>(
+    <Layout>
+        <ThongDiep></ThongDiep>
+        <ChiaSe></ChiaSe>
+    </Layout>
+);
+
+export default Index;
