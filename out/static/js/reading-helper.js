541cc699c85498e99c3342d3208b54f9d9a59b58
Long <long.la@clearlane.com>
2019-07-20 00:21:12 -0700

    deploy

---
+++ out/static/js/reading-helper.js
@@ -0,0 +1,27 @@
+
+function toggleReadingPanel(panelElement) {
+    const hideClassName = "hide";
+    if (panelElement.classList.contains(hideClassName)) {
+        panelElement.classList.remove(hideClassName);
+    }
+    else {
+        panelElement.classList.add(hideClassName);
+    }
+}
+
+function readingBtnClickHandle() {
+    // handle click event
+    const btn = document.getElementById('reading-helper-action');
+    const readingPanel = document.querySelector('.reading-action-wrapper');
+    btn.addEventListener('click', () => {
+        toggleReadingPanel(readingPanel);
+    });
+}
+
+function init() {
+    document.addEventListener('DOMContentLoaded', () => {
+        readingBtnClickHandle();
+    })
+}
+
+init();
