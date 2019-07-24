9486e966d67e485b463bf948bcfe59806d39e0a0
Long <long.la@clearlane.com>
2019-07-23 23:54:25 -0700

    init blog

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const withSass = require('@zeit/next-sass')
+const withSass = require("@zeit/next-sass");
 module.exports = withSass({
   /* config options here */
-})
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    });
+    return config;
+  }
+});
