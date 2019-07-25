7265059b57c8423e03cfb40fe32e65acb62554c7
LONG LA <baolonguit@gmail.com>
2019-07-25 06:21:20 +0000

    updates

--- next.config.js
+++ next.config.js
@@ -1,42 +1,45 @@
 const withSass = require("@zeit/next-sass");
 const postService = require("./postService");
 const APP_ROUTES = {
-    '/blog':{
-                page:'/blog'
-          },
-           'landing':{
-                page:'landing'
-          },
-           'chiase':{
-                page:'chiase'
-          },
-           'thongdiep':{
-                page:'thongdiep'
-          },
-          '404':{
-                page:'404'
-          },
-          'index':{
-                page:'index'
-          }
+    '/blog': {
+        page: '/blog'
+    },
+    '/annoucement': {
+        page: '/annoucement'
+    },
+    'landing': {
+        page: 'landing'
+    },
+    'chiase': {
+        page: 'chiase'
+    },
+    'thongdiep': {
+        page: 'thongdiep'
+    },
+    '404': {
+        page: '404'
+    },
+    'index': {
+        page: 'index'
+    }
 }
 
 module.exports = withSass({
-  /* config options here */
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader"
-    });
-    return config;
-  },
-  exportPathMap:function(){
-      const blogRoutes = postService.getBlogPostRoutes();
-      const annoucementRoutes = postService.getAnnoucementPostRoutes();
-      return {
-          ...APP_ROUTES,
-          ...blogRoutes,
-          ...annoucementRoutes
-      }
-  }
+    /* config options here */
+    webpack: config => {
+        config.module.rules.push({
+            test: /\.md$/,
+            use: "raw-loader"
+        });
+        return config;
+    },
+    exportPathMap: function() {
+        const blogRoutes = postService.getBlogPostRoutes();
+        const annoucementRoutes = postService.getAnnoucementPostRoutes();
+        return {
+            ...APP_ROUTES,
+            ...blogRoutes,
+            ...annoucementRoutes
+        }
+    }
 });
