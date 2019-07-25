6011510bdfc9d9ce9d52357ca233563f8e9e7e12
LONG LA <baolonguit@gmail.com>
2019-07-25 14:14:17 +0000

    update nav logic, use react

--- components/header.js
+++ components/header.js
@@ -1,22 +1,43 @@
+import React from 'react';
 
-const Header = () => (
-    <div className={"app-nav"}>
-        <div className={"nav-bar"}>
-            <div>
-                <a className={'page-icon'} href="/">DSCN</a>
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isNavShown: false };
+        // This binding is necessary to make `this` work in the callback
+        this.navToggleClickHandler = this.navToggleClickHandler.bind(this);
+    }
+    navToggleClickHandler() {
+
+        this.setState({
+            isNavShown: !this.state.isNavShown
+        });
+    }
+    render() {
+        const isNavShown = this.state.isNavShown;
+        const appNavPanel = (<nav>
+                        <a href="/thongdiep" className={"nav-item"}>Thông Điệp</a>
+                        <a href="/chiase" className={"nav-item"}>Chia Sẻ</a>
+                        <a href="/blog" className={"nav-item"}>Blog</a>
+                        <a href="/annoucement" className={"nav-item"}>Thông Báo</a>
+                    </nav>);
+        return (
+
+            <div className={"app-nav"}>
+                <div className={"nav-bar"}>
+                    <div>
+                        <a className={'page-icon'} href="/">DSCN</a>
+                    </div>
+                    <a onClick={this.navToggleClickHandler} className={'nav-toggle'}>
+                        <i className={"fas fa-bars icon-show"} />
+                    </a>
+
+                </div>
+                {
+                    isNavShown ? appNavPanel : null
+                }
+
             </div>
-            <a className={'nav-toggle'}>
-                <i className={"fas fa-bars icon-show"} />
-            </a>
-
-        </div>
-        <nav>
-            <a href="/thongdiep" className={"nav-item"}>Thông Điệp</a>
-            <a href="/chiase" className={"nav-item"}>Chia Sẻ</a>
-            <a href="/blog" className={"nav-item"}>Blog</a>
-            <a href="/annoucement" className={"nav-item"}>Thong Bao</a>
-        </nav>
-    </div>
-
-);
-export default Header;
+        )
+    }
+}
