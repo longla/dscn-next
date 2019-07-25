9ef922b5bb90bb7c3bee9528a1351c614b8df5aa
LONG LA <baolonguit@gmail.com>
2019-07-25 07:02:51 +0000

    udpate app-nav styles

--- components/header.js
+++ components/header.js
@@ -1,15 +1,22 @@
 
 const Header = () => (
-  <div className={"nav-bar"}>
-    <div>
-      <a className={'page-icon'} href="/">DSCN</a>
+    <div className={"app-nav"}>
+        <div className={"nav-bar"}>
+            <div>
+                <a className={'page-icon'} href="/">DSCN</a>
+            </div>
+            <a className={'nav-toggle'}>
+                <i className={"fas fa-bars icon-show"} />
+            </a>
+
+        </div>
+        <nav>
+            <a href="/thongdiep" className={"nav-item"}>Thông Điệp</a>
+            <a href="/chiase" className={"nav-item"}>Chia Sẻ</a>
+            <a href="/blog" className={"nav-item"}>Blog</a>
+            <a href="/annoucement" className={"nav-item"}>Thong Bao</a>
+        </nav>
     </div>
-    <nav>
-      <a href="/thongdiep" className={"nav-item"}>Thông Điệp</a>
-      <a  href="/chiase" className={"nav-item"}>Chia Sẻ</a>
-      <a  href="/blog" className={"nav-item"}>Blog</a>
-      <a  href="/annoucement" className={"nav-item"}>Thong Bao</a>
-    </nav>
-  </div>
+
 );
 export default Header;
